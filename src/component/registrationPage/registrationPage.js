9a7920bef4f4bd9731b34e8397d358cca9d2f97a
Krima222 <manackaakristina@gmail.com>
2022-05-13 20:22:22 +0300

    create blanks for components

---
+++ src/component/registrationPage/registrationPage.js
@@ -0,0 +1,14 @@
+import './registrationPage.scss'
+
+
+const RegistrationPage = () => {
+    return (
+        <div className="registration-modal">
+            <div className="registration-dialog">
+
+            </div>
+        </div>
+    )
+}
+
+export default RegistrationPage;
