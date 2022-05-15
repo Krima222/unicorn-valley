348d7f795b42ad6a006788c405ba03a723d97ac4
Krima222 <manackaakristina@gmail.com>
2022-05-15 23:27:55 +0300

    add login and heder

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
-import RegistrationPage from './component/registrationPage/registrationPage';
-
+import FormPage from './component/pages/formPage/formPage';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Registration from './component/registration/registration';
+import Login from './component/login/login'
+import Header from './component/header/header';
 
 function App() {
   return (
-    <>
-      <RegistrationPage/>
-    </>
+    <Router>
+        <Routes>
+            <Route path='/form' element={<FormPage/>}>
+                <Route path='registration' element={<Registration/>}/>
+                <Route path='login' element={<Login/>}/>
+            </Route>
+            <Route path='/homepage' element={<Header/>}/>
+        </Routes>
+    </Router>
   );
 }
 
