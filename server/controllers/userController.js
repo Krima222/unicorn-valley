11f1e994844fabb7d3f3b099ba9275857925d725
Krima222 <manackaakristina@gmail.com>
2022-05-22 21:37:16 +0300

    add validation in registration

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,8 +1,13 @@
-import User from '../models/user'
+import User from '../models/user.js'
 import bcrypt from 'bcryptjs'
+import { validationResult } from 'express-validator'
 
 export const registrate = async (req, res) => {
     try {
+        const errors = validationResult(req)
+        if(!errors.isEmpty()) {
+            return res.status(400).json({message: 'Ошибка при регистрации', errors})
+        }
         const {nickname, email, password} = req.body
         const condidate = await User.findOne({ $or: [{nickname}, {email}]})
         if (condidate) {
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,9 +1,14 @@
 import express from "express";
 
-import { registrate } from "../controllers/userController";
+import { registrate } from '../controllers/userController.js';
+import { check } from 'express-validator'
 
 const userRouter = express.Router();
 
-userRouter.post('/registration', registrate);
+userRouter.post('/registration', [
+    check('nickname', 'Имя пользователя не должно быть пустым, идите в лес').notEmpty(),
+    check('email', 'Email пользователя не должен быть пустым, идите в лес').notEmpty(),
+    check('password', 'Пороль не может быть меньше 4 и больше 10 символов').isLength({min: 4, max: 10})
+], registrate);
 
 export default userRouter;
