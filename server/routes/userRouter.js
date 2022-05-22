31081d2685e619cabfa8761fbb3755800a4e4a19
Krima222 <manackaakristina@gmail.com>
2022-05-22 22:54:41 +0300

    add authMiddleware

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,7 +1,10 @@
 import express from "express";
 
 import { registrate } from '../controllers/userController.js';
+import { login } from "../controllers/userController.js";
+import { getUsers } from "../controllers/userController.js";
 import { check } from 'express-validator'
+import {authMiddleware} from '../middleware.js'
 
 const userRouter = express.Router();
 
@@ -10,5 +13,7 @@ userRouter.post('/registration', [
     check('email', 'Email пользователя не должен быть пустым, идите в лес').notEmpty(),
     check('password', 'Пороль не может быть меньше 4 и больше 10 символов').isLength({min: 4, max: 10})
 ], registrate);
+userRouter.post('/login', login);
+userRouter.get('/users', authMiddleware, getUsers)
 
 export default userRouter;
