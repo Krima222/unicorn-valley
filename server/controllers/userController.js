6750cbe426b99c62b5cc72cceff33023819ae33d
Krima222 <manackaakristina@gmail.com>
2022-05-22 22:21:23 +0300

    server add login

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,11 +1,20 @@
 import User from '../models/user.js'
 import bcrypt from 'bcryptjs'
 import { validationResult } from 'express-validator'
+import jwt from 'jsonwebtoken'
+import {secret} from './config'
+
+const generateAccessToken = (id) => {
+    const payload = {
+        id
+    }
+    return jwt.sign(payload, secret, {expiresIn: '24h'})
+}
 
 export const registrate = async (req, res) => {
     try {
         const errors = validationResult(req)
-        if(!errors.isEmpty()) {
+        if (!errors.isEmpty()) {
             return res.status(400).json({message: 'Ошибка при регистрации', errors})
         }
         const {nickname, email, password} = req.body
@@ -19,5 +28,24 @@ export const registrate = async (req, res) => {
         return res.json({message: 'Пользователь успешно зарегестрирован'})
     } catch (e) {
         res.status(400).json({message: 'Registration error'})
+    }
+}
+
+export const login = async (req, res) => {
+    try {
+        const {nickname, password} = req.body
+        const user = await User.findOne({nickname})
+        if (!user) {
+            return res.status(400).json({message: `Пользователь ${nickname} не найден, иди в лес`})
+        }
+        const validingPassword = bcrypt.compareSync(password, user.password)
+        if (!validingPassword) {
+            return res.status(400).json({message: `Введён неверный пароль`})
+        }
+        const token = generateAccessToken(user._id)
+        return res.json({token})
+    } catch (e) {
+        console.log(e)
+        res.status(400).json({message: 'Login error'})
     }
 }
