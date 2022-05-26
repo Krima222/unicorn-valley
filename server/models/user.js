6886f85c4abb87751af41ceec35f4acaad991e85
Krima222 <manackaakristina@gmail.com>
2022-05-26 16:17:55 +0300

    add userRoute game

--- server/models/user.js
+++ server/models/user.js
@@ -3,7 +3,8 @@ import mongoose from 'mongoose'
 const user = new mongoose.Schema({
     nickname: {type: String, unique: true, required: true},
     email: {type: String, required: true},
-    password: {type: String, required: true}
+    password: {type: String, required: true},
+    game: {}
 }, {timestamps: true})
 
 export default mongoose.model('User', user)
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,10 +1,8 @@
 import express from "express";
 
-import { registrate } from '../controllers/userController.js';
-import { login } from "../controllers/userController.js";
-import { getUsers } from "../controllers/userController.js";
-import { check } from 'express-validator'
-import { authMiddleware } from '../middlewares/authMiddleware.js'
+import { registrate, login, getUsers, getGameData, updateGameData } from '../controllers/userController.js';
+import { check } from 'express-validator';
+import { authMiddleware } from '../middlewares/authMiddleware.js';
 
 const userRouter = express.Router();
 
@@ -14,6 +12,9 @@ userRouter.post('/registration', [
     check('password', 'Пороль не может быть меньше 4 и больше 10 символов').isLength({min: 4, max: 10})
 ], registrate);
 userRouter.post('/login', login);
-userRouter.get('/users', authMiddleware, getUsers)
+userRouter.get('/users', authMiddleware, getUsers);
+
+userRouter.get('/game', getGameData);
+userRouter.put('/game', updateGameData);
 
 export default userRouter;
